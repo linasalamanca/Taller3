0f8f6b84b73a73db1f62d3ac2af780d5af4caa06
SailorMusic05 <141599762+SailorMusic05@users.noreply.github.com>
2023-09-18 22:43:43 -0500

    Cambios Main
    Un Main que aún no funciona XD, pero tiene lo que nos mandó el profe

--- main.cpp
+++ main.cpp
@@ -1,250 +1,152 @@
-#include "ArbolBinarioOrdenado.h"
-#include "NodoBinario.h"
-#include <queue>
+#include <ctime>
 #include <iostream>
+#include <fstream>
+#include <string>
 
-template< class T >
-ArbolBinarioOrdenado<T>::ArbolBinarioOrdenado(){
-    this->raiz = nullptr;
-}
+#include <list>
+#include <set>
+// TODO #1: incluir cabeceras implementaciones propias
+#include "ArbolBinarioOrdenado.h"
+// #include "---> implementacion_ArbolAVL.h <---"
 
-template< class T >
-ArbolBinarioOrdenado<T>::~ArbolBinarioOrdenado(){
-    if(this->raiz != nullptr){
-        delete this->raiz;
-        this->raiz = nullptr;
+// -------------------------------------------------------------------------
+typedef std::list< std::string > TList;
+typedef std::set< std::string >  TSet;
+// TODO #2: definir tipos de datos para arboles de cadenas de caracteres
+typedef ArbolBinarioOrdenado<std::string> TArbolBO;
+// typedef implementacion_ArbolAVL< std::string > TArbolAVL;
+
+// -------------------------------------------------------------------------
+template< class TTree >
+bool ReadTree( TTree& tree, const std::string& filename );
+
+// -------------------------------------------------------------------------
+int main( int argc, char* argv[] ) {
+
+    if( argc < 2 ) {
+        std::cerr << "Uso: " << argv[ 0 ] << " archivo_entrada" << std::endl;
+        return( -1 );
     }
 
-}
+    // TODO #3: declarar arboles
+    TArbolBO arbolBO;
+    //TArbolAVL arbolAVL;
+    //TSet arbolRN;
 
-template< class T >
-bool ArbolBinarioOrdenado<T>::esVacio(){
-    return this->raiz==nullptr;
-}
+    // Llenar arbol binario ordenado y obtener tiempo de ejecucion
+    std::clock_t start_arbolBO = std::clock( );
+    // TODO #4: llenar arbol desde archivo con funcion ReadTree
+    bool llenar_arbolBO = ReadTree( arbolBO, argv[ 1 ] );
+    std::clock_t end_arbolBO = std::clock( );
+    double tiempo_arbolBO =
+            ( end_arbolBO - start_arbolBO ) / double( CLOCKS_PER_SEC );
+    //TODO #5: si se pudo llenar el arbol, imprimir el tiempo
+       if( llenar_arbolBO )
+         std::cout
+           << "Tiempo de llenado Arbol Binario Ordenado = "
+           << tiempo_arbolBO << "seg."
+           << std::endl;
+       else
+         std::cout
+           << "Error al usar \"" << argv[ 1 ]
+           << "\" para llenar el arbol binario ordenado."
+           << std::endl;
 
-template< class T >
-T ArbolBinarioOrdenado<T>::datoRaiz(){
-    return (this->raiz)->obtenerDato();
-}
-//recurrente
-template< class T >
-int ArbolBinarioOrdenado<T>::altura(){
-    if(this->esVacio())
-        return -1;
+    // Llenar arbol AVL y obtener tiempo de ejecucion
+    //std::clock_t start_arbolAVL = std::clock( );
+    // TODO #6: llenar arbol AVL desde archivo con funcion ReadTree
+    // bool llenar_arbolAVL = ReadTree( arbolAVL, argv[ 1 ] );
+    //std::clock_t end_arbolAVL = std::clock( );
+    //double tiempo_arbolAVL =
+    //        ( end_arbolAVL - start_arbolAVL ) / double( CLOCKS_PER_SEC );
+    /* TODO #7: si se pudo llenar el arbol, imprimir el tiempo
+       if( llenar_arbolAVL )
+         std::cout
+           << "Tiempo de llenado Arbol AVL = "
+           << tiempo_arbolAVL << "seg."
+           << std::endl;
+       else
+         std::cout
+           << "Error al usar \"" << argv[ 1 ]
+           << "\" para llenar el arbol AVL."
+           << std::endl;
+    */
+
+    // Llenar arbol RN y obtener tiempo de ejecucion
+    /*std::clock_t start_arbolRN = std::clock( );
+    bool llenar_arbolRN = ReadTree( arbolRN, argv[ 1 ] );
+    std::clock_t end_arbolRN = std::clock( );
+    double tiempo_arbolRN =
+            ( end_arbolRN - start_arbolRN ) / double( CLOCKS_PER_SEC );
+    if( llenar_arbolRN )
+        std::cout
+                << "Tiempo de llenado Arbol RN = "
+                << tiempo_arbolRN << "seg."
+                << std::endl;
     else
-        return this-> altura(this->raiz);
+        std::cout
+                << "Error al usar \"" << argv[ 1 ]
+                << "\" para llenar el arbol RN."
+                << std::endl;
+
+    // Obtener recorrido en inorden del arbol binario ordenado
+    TList inorden_arbolBO;*/
+    // TODO #8: usar funcion del arbol para obtener recorrido en lista
+    //arbolBO.inOrdenLista( inorden_arbolBO );
+    /*arbolBO.inOrden( inorden_arbolBO );
+
+    // Obtener recorrido en inorden del arbol AVL
+    TList inorden_arbolAVL;
+    // TODO #9: usar funcion del arbol AVL para obtener recorrido en lista
+    // arbolAVL.inOrdenLista( inorden_arbolAVL );
+
+    if( inorden_arbolBO.size( ) != inorden_arbolAVL.size( ) ||
+        inorden_arbolBO.size( ) != arbolRN.size( ) ||
+        inorden_arbolAVL.size( ) != arbolRN.size( )) {
+
+        std::cout << "Tamannos de los arboles no coinciden." << std::endl;
+        return( -1 );
+    }*/
+
+    // Comparar los arboles
+    /*TList::const_iterator aboIt = inorden_arbolBO.begin( );
+    TList::const_iterator avlIt = inorden_arbolAVL.begin( );
+    TSet::const_iterator rbIt = arbolRN.begin( );
+    bool equal = true;
+    for( ; rbIt != arbolRN.end( ); aboIt++, avlIt++, rbIt++ )
+        equal &= ( *rbIt == *avlIt && *avlIt == *aboIt && *rbIt == *aboIt );*/
+
+    // Informar si los arboles coinciden
+    /*std::cout << "Los arboles ";
+    if( !equal )
+        std::cout << "no ";
+    std::cout << "coinciden." << std::endl;*/
+
+    return( 0 );
 }
 
-template< class T >
-int ArbolBinarioOrdenado<T>::altura(NodoBinario<T>* nodo){
-    int valt;
+// -------------------------------------------------------------------------
+template< class TTree >
+bool ReadTree( TTree& tree, const std::string& filename ) {
 
-    if(nodo->esHoja()){
-        valt = 0;
-    }else{
-        int valt_izq = -1;
-        int valt_der = -1;
+    std::ifstream input( filename.c_str( ) );
+    if( !input )
+        return( false );
 
-        if(nodo->obtenerHijoIzq() != nullptr)
-            valt_izq = this -> altura(nodo->obtenerHijoIzq());
-        if(nodo->obtenerHijoDer() != nullptr)
-            valt_der = this -> altura(nodo->obtenerHijoDer());
-        if(valt_izq > valt_der)
-            valt = valt_izq +  1;
-        else
-            valt = valt_der + 1;
+    while( !input.eof( ) ) {
+
+        std::string code, value;
+        input >> code >> value;
+        if( code == "add" )
+            tree.insert( value );  // El arbol debe proveer el metodo "insert"
+        else if( code == "del" )
+            tree.erase( value );  // El arbol debe proveer el metodo "erase"
+
     }
 
-    return valt;
-}
-//recurrente
-template<class T>
-int ArbolBinarioOrdenado<T>::tamano() {
-    return tamano(this->raiz);
+    input.close( );
+    return( true );
 }
 
-template<class T>
-int ArbolBinarioOrdenado<T>::tamano(NodoBinario<T>* nodo) {
-    if (nodo == nullptr) {
-        return -1;
-    } else {
-        return 1 + tamano(nodo->obtenerHijoIzq()) + tamano(nodo->obtenerHijoDer());
-    }
-}
-
-//iterativa
-template< class T >
-bool ArbolBinarioOrdenado<T>::insertar(T val){
-    NodoBinario<T>* nodo = this->raiz;
-    NodoBinario<T>* padre = this->raiz;
-    bool insertado = false;
-    bool duplicado = false;
-
-    while (nodo!=nullptr){
-        padre = nodo;
-        if(val < nodo->obtenerDato()){
-            nodo = nodo->obtenerHijoIzq();
-        } else if(val > nodo->obtenerDato()){
-            nodo = nodo->obtenerHijoDer();
-        }else{
-            duplicado = true;
-            break;
-        }
-    }
-
-    if(!duplicado){
-        NodoBinario<T>* nuevo = new NodoBinario<T>(val);
-        if(nuevo!=nullptr){
-            if(val < padre->obtenerDato())
-                padre->fijarHijoIzq(nuevo);
-            else
-                padre->fijarHijoDer(nuevo);
-        }
-        insertado = true;
-    }
-    return insertado;
-}
-//iterativa
-/*template< class T >
-ArbolBinarioOrdenado<T>::eliminar(T val){
-    //comparar con dato en nodo para bajar por izquierda o derecha
-    //y para saber si val está en el árbol
-
-    //Si val está en el árbol y ya baje
-    //verificar situación de eliminación
-    //1. Nodo hoja, borrarlo
-    //2. Nodo con un solo hijo, usar hijo para reemplazar nodo
-    //3. Nodo con dos hijos, usar elmaximo del subarbol izquierdo
-    //   para reemplazazr nodo
-}*/
-
-template<class T>
-bool ArbolBinarioOrdenado<T>::eliminar(T val) {
-    NodoBinario<T>* nodo = this->raiz;
-    NodoBinario<T>* padre = this->raiz;
-
-    while (nodo != nullptr && nodo->obtenerDato() != val) {
-        padre = nodo;
-        if (val < nodo->obtenerDato())
-            nodo = nodo->obtenerHijoIzq();
-        else
-            nodo = nodo->obtenerHijoDer();
-    }
-
-    if (nodo == nullptr) {
-        return false;
-    }
-
-    //3. Nodo con dos hijos, usar elmaximo del subarbol izquierdo
-    //   para reemplazazr nodo
-    if (nodo->obtenerHijoIzq() != nullptr && nodo->obtenerHijoDer() != nullptr) {
-        NodoBinario<T>* sucesor = nodo->obtenerHijoDer();
-        NodoBinario<T>* sucesorPadre = nodo;
-
-        while (sucesor->obtenerHijoIzq() != nullptr) {
-            sucesorPadre = sucesor;
-            sucesor = sucesor->obtenerHijoIzq();
-        }
-
-        nodo->fijarDato(sucesor->obtenerDato());
-
-        nodo = sucesor;
-        padre = sucesorPadre;
-    }
-
-   //2. Nodo con un solo hijo, usar hijo para reemplazar nodo
-    NodoBinario<T>* hijo;
-    if (nodo->obtenerHijoIzq() != nullptr) {
-        hijo = nodo->obtenerHijoIzq();
-    } else {
-        hijo = nodo->obtenerHijoDer();
-    }
-
-    // 1. Nodo hoja, borrarlo
-    if (padre == nullptr) {
-        this->raiz = hijo;
-    } else {
-        if (nodo == padre->obtenerHijoIzq())
-            padre->fijarHijoIzq(hijo);
-        else
-            padre->fijarHijoDer(hijo);
-    }
-
-    delete nodo;
-    return true;
-}
-
-//iterativa
-template< class T >
-bool ArbolBinarioOrdenado<T>::buscar(T val){
-    NodoBinario<T>* nodo = this->raiz;
-    bool encontrado = false;
-
-    while (nodo!=nullptr && !encontrado){
-        if(val < nodo->obtenerDato()){
-            nodo = nodo->obtenerHijoIzq();
-        } else if(val > nodo->obtenerDato()){
-            nodo = nodo->obtenerHijoDer();
-        }else{
-            encontrado = true;
-        }
-    }
-
-    return encontrado;
-}
-//recurrente
-template< class T >
-void ArbolBinarioOrdenado<T>::preOrden(NodoBinario<T>* nodo){
-    if(nodo!=nullptr){
-        std::cout << nodo->obtenerDato()<<" ";
-        this->preOrden(nodo->obtenerHijoIzq());
-        this->preOrden(nodo->obtenerHijoDer());
-    }
-
-}
-//recurrente
-template< class T >
-void ArbolBinarioOrdenado<T>::inOrden(){
-    if(!this->esVacio())
-        this->inOrden(this->raiz);
-}
-
-template< class T >
-void ArbolBinarioOrdenado<T>::inOrden(NodoBinario<T>* nodo){
-    if(nodo!=nullptr){
-        this->inOrden(nodo->obtenerHijoIzq());
-        std::cout << nodo->obtenerDato()<<" ";
-        this->inOrden(nodo->obtenerHijoDer());
-    }
-}
-//Recurrente
-template< class T >
-void ArbolBinarioOrdenado<T>::posOrden(NodoBinario<T>* nodo){
-    if(nodo!=nullptr){
-        this->posOrden(nodo->obtenerHijoIzq());
-        this->posOrden(nodo->obtenerHijoDer());
-        std::cout << nodo->obtenerDato()<<" ";
-    }
-
-}
-//iterativo
-template< class T >
-void ArbolBinarioOrdenado<T>::nivelOrden(){
-    if(!this->esVacio()){
-        std::queue<NodoBinario<T>*>cola;
-        cola.push(this->raiz);
-        NodoBinario<T>* nodo;
-        while(!cola.empty()){
-            nodo = cola.front();
-            cola.pop();
-            std::cout<< nodo->obtenerDato() << " ";
-            if(nodo->obtenerHijoIzq != nullptr){
-                cola.push(nodo->obtenerHijoIzq());
-            }
-            if(nodo->obtenerHijoDer != nullptr){
-                cola.push(nodo->obtenerHijoDer());
-            }
-        }
-    }
-}
+// eof - taller_3_ordenamiento_busqueda.cxx
 
